4de84ae4f8a286275902b3d510d4d99a8d404a86
Jasmeh Singh <142116700+jasmehsidhu@users.noreply.github.com>
2026-08-08 11:18:56 -0400

    Update script.js

--- Backend/script.js
+++ Backend/script.js
@@ -1,40 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import nodemailer from 'nodemailer'
+import { Resend } from 'resend';
+
 
 const port = process.env.PORT || 1000
 const app = express()
+const resend = new Resend('re_6YHj8SbR_P9R8pgDRauktDK1KKciTXsBU');
+resend.apiKeys.create({ name: 'Production' });
 app.use(cors())
 app.use(express.json())
 app.listen(port, () => {
     console.log('Server Started')
 })
 
-app.post('/', (req, res) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'jasmehasofficial@gmail.com',
-            pass: 'xikd kasi rpmj qqtq'
-        }
-    });
+app.post('/',async (req, res) => {
+  const { data, error } = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: 'jasmeh76@gmail.com',
+            subject: `Message from ${req.body.username}`,
+            text: `Email : ${req.body.email} , ${req.body.message}`
+        })
 
-    const mailOptions = {
-        from: 'jasmehasofficial@gmail.com',
-        to: "jasmeh82@gmail.com",
-        subject: `Message from ${req.body.username}`,
-        text: `Email : ${req.body.email} , ${req.body.message}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            return res.status(500).json({ success: false, error: error.message });
-        } else {
-            console.log("Email sent: " + info.response);
-            return res.status(200).json({ success: true });
-        }
-    });
+  
 })
